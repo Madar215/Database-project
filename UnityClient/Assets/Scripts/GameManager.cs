d6aeb1cd9806095cc52603566f8a29908d7d75cb
Madar215 <galmadar2424@gmail.com>
2025-08-06 16:27:19 +0300

    Some Work

--- UnityClient/Assets/Scripts/GameManager.cs
+++ UnityClient/Assets/Scripts/GameManager.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 
 [System.Serializable]
-public class Question
-{
+public class Question {
     public string questionText;
     public string answer1Text;
     public string answer2Text;
@@ -12,29 +12,75 @@ public class Question
     public string answer4Text;
     public int correctAnswer;
 }
-public class GameManager : MonoBehaviour
-{
-    public int numberOfQuestion;
 
-    private void Start()
-    {
-        StartCoroutine(GetText());
+public class GameManager : MonoBehaviour {
+    [Header("References")] 
+    [SerializeField] private UiManager uiManager;
+
+    [Header("Timer")] 
+    [SerializeField] private float timerDuration = 10f;
+    
+    // Properties
+    public Question[] Questions { get; set; }
+    
+    // Events
+    public event UnityAction<Question> OnRoundStart;
+    
+    // Timer
+    private CountdownTimer _roundTimer;
+    
+    // Questions
+    private int _curQuestionIndex;
+
+    private void Awake() {
+        _roundTimer = new CountdownTimer(timerDuration);
     }
 
-    private IEnumerator GetText()
-    {
-        var www = UnityWebRequest.Get("https://localhost:7057/api/Trivia");
+    private void OnEnable() {
+        _roundTimer.OnTimerStart += StartRound;
+        _roundTimer.OnTimerStop += EndRound;
+    }
+
+    private void OnDisable() {
+        _roundTimer.OnTimerStart -= StartRound;
+        _roundTimer.OnTimerStop -= EndRound;
+    }
+
+    private void Start() {
+        StartCoroutine(GetQuestions());
+    }
+
+    private void Update() {
+        _roundTimer.Tick(Time.deltaTime);
+    }
+
+    private void StartRound() {
+        if (Questions.Length > 0 && _curQuestionIndex < Questions.Length)
+            OnRoundStart?.Invoke(Questions[_curQuestionIndex]);
+        else
+            GameOver();
+    }
+
+    private void EndRound() {
+        // TODO: Implement Round End
+    }
+
+    private void GameOver() {
+        // TODO: Implement Game Over
+    }
+
+    private IEnumerator GetQuestions() {
+        var www = UnityWebRequest.Get("http://localhost:5195/api/Trivia");
         yield return www.SendWebRequest();
 
-        if (www.result == UnityWebRequest.Result.ConnectionError)
-        {
+        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError) {
             Debug.Log(www.error);
-        }
-        else
-        {
-            // Show results as text
-            Debug.Log(www.downloadHandler.text);
-            //UpdateQuestionUI(www.downloadHandler.text);
+        } else {
+            var json = www.downloadHandler.text;
+
+            // Store it for later
+            Questions = JsonHelper.FromJson<Question>(json);
+            _roundTimer.Start();
         }
     }
 }
--- UnityClient/Assets/Scripts/UiManager.cs
+++ UnityClient/Assets/Scripts/UiManager.cs
@@ -2,20 +2,29 @@ using TMPro;
 using UnityEngine;
 
 public class UiManager : MonoBehaviour {
+    [Header("References")] 
+    [SerializeField] private GameManager gameManager;
+    
     [Header("Questions")] 
     [SerializeField] private TextMeshProUGUI questionText;
     [SerializeField] private TextMeshProUGUI answer1Text;
     [SerializeField] private TextMeshProUGUI answer2Text;
     [SerializeField] private TextMeshProUGUI answer3Text;
     [SerializeField] private TextMeshProUGUI answer4Text;
-    
-    private void UpdateQuestionUI(string json)
-    {
-        var curQuestion = JsonUtility.FromJson<Question>(json);
-        questionText.text = curQuestion.questionText;
-        answer1Text.text = curQuestion.answer1Text;
-        answer2Text.text = curQuestion.answer2Text;
-        answer3Text.text = curQuestion.answer3Text;
-        answer4Text.text = curQuestion.answer4Text;
+
+    private void OnEnable() {
+        gameManager.OnRoundStart += UpdateQuestionUI;
+    }
+
+    private void OnDisable() {
+        gameManager.OnRoundStart-= UpdateQuestionUI;
+    }
+
+    private void UpdateQuestionUI(Question question) {
+        questionText.text = question.questionText;
+        answer1Text.text = question.answer1Text;
+        answer2Text.text = question.answer2Text;
+        answer3Text.text = question.answer3Text;
+        answer4Text.text = question.answer4Text;
     }
 }
